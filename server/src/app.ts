0715a46d001fc02286d9f4730e91c015dc060fe5
nidhinm <nidhinmohannidhin@gmail.com>
2025-06-23 08:10:36 +0530

    Dockerised react

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,14 +8,26 @@ import { errorHandler, notFound } from "./middlewares/errorMiddleware";
 
 const app: Application = express();
 
-// dotenv.config();
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace with your frontend origin
+  credentials: true, // Allow credentials
+};
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
+
+// Middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 
 // Routes
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to Moodmate",
+  });
+})
 app.use("/auth", authRoutes);
 
 // Error handling
