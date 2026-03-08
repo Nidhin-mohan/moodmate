f3b98995128cb1413d8eb664f4761add221e4fd9
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2026-03-08 12:21:51 +0530

    update docker donfig for diployment

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,7 @@
 import { showToast } from "@/utils/toast";
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api/v1";
 
 // Create an Axios instance
 const api = axios.create({
